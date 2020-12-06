7b24aa673ca8a3b8816d3f55528377466b748309
cmbuckley93 <buckl123@msu.edu>
2020-12-06 14:49:59 -0800

    Take 2

--- scripts/ZoopPlot_FInal.R
+++ scripts/ZoopPlot_FInal.R
@@ -1,2 +1,12 @@
 #12/6/20
 # Plot data
+
+{
+  
+  rm(list=ls());
+  options(show.error.locations=TRUE);
+  
+  bass <- read.csv("data/whitebass_final.csv");
+  sucker <- read.csv("data/whitesucker_final.csv");
+  zooplank <- read.csv("data/zooplankton_final.csv")l
+  
