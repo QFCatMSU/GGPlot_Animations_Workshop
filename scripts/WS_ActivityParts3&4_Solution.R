91f555665b33dbf2f8a5a4a967e2cfe0087d9e65
userName <email>
2021-01-24 14:40:11 -0500

    Changed format for pt 3&4 assignment

--- scripts/WS_ActivityParts3&4_Solution.R
+++ scripts/WS_ActivityParts3&4_Solution.R
@@ -10,14 +10,21 @@
   # Activity #
   ############
   
-  # This time, we’ll be using the larval white sucker data. Subset the data set 
-  # from April to September. Plot month vs zooplankton and larval white sucker 
-  # abundance, and create a separate plot for each year by faceting. Add a second 
-  # axis for the white sucker abundance (you will need to calculate a new scaling 
-  # coefficient here, using the same method that was used to calculate the scaling 
-  # coefficient for the larval white bass data). Change the color of the 
-  # lines and the theme of the plot as you see fit, making sure to reflect this in 
-  # the legend. 
+  # This time, we’ll be using the larval white sucker data. 
+  #
+  #   (1) Subset the data set from April to September.
+  #
+  #   (2) Plot month vs zooplankton and larval white sucker abundance.
+  #
+  #   (3) Create a separate plot for each year by faceting.
+  #
+  #   (4) Add a second axis for the white sucker abundance; you will need 
+  #       to calculate a new scaling coefficient here, using the same method 
+  #       that was used to calculate the scaling coefficient for the larval 
+  #       white bass data. 
+  #
+  #   (5) Change the color of the lines and the theme of the plot as you see 
+  #       fit, making sure to reflect this in the legend. 
   
   
   ############
