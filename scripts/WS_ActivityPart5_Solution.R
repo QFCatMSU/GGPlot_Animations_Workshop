03a2c61d09a00ea9e3ae86c0978fd3d5c3bed70c
cmbuckley93 <buckl123@msu.edu>
2021-01-25 10:25:13 -0700

    Updates based on Charlie's comments

--- scripts/WS_ActivityPart5_Solution.R
+++ scripts/WS_ActivityPart5_Solution.R
@@ -1,7 +1,9 @@
 {
+  # PART 5 ACTIVITY -- ANIMATION
+  
   rm(list=ls());                         # clear Console Window
   options(show.error.locations = TRUE);  # show line numbers on error
-
+  
   library(package=ggplot2);
   library(package=gganimate);
   library(package=av);          
@@ -9,52 +11,58 @@
   library(package=gifski);      
   library(package=transformr);  
   
-  # PART 5 ACTIVITY -- ANIMATION
+  # TASKS
+  # 1. Animate the plot by day
+  # 2. Change the plot to animate by month
+  # 3. Change the plot to animate by season
+  # 4. Change the plot so that the point size varies with precipitation
+  # 5. Change the plot so that the color varies with the transition state
+  # 6. Adjust the nframes argument. What happens to your plot output?
+  # 7. Adjust the fps argument. What happens to your plot output?
   
   weatherData = read.csv(file="data/LansingNOAA2016-3.csv", 
                          stringsAsFactors = FALSE);
   
-  # First, make sure your grouping factors are in the correct order
-  colnames(weatherData)[1] <- "day"
-  weatherData$month <- ordered(weatherData$month, 
-                               levels=month.name)
-  weatherData$season <- ordered(weatherData$season, 
-                                levels=c("Winter","Spring","Summer","Fall"))
+  colnames(weatherData)[1] = "day";                           # Rename the column of rownumbers to refer to the "day"
+  weatherData$month = ordered(weatherData$month,              # First, make sure your grouping factors are in the correct order
+                              levels=month.name);
+  weatherData$season = ordered(weatherData$season, 
+                               levels=c("Winter","Spring","Summer","Fall"));
   
-  plot5a = ggplot(data=weatherData) +
-    geom_point(mapping=aes(x=avgTemp, y=relHum, group = 1L , color = season ))+
-    labs(title = paste('Humidity (y) vs. Temperature (x) by Season (animation)'),
+  plot5c = ggplot(data=weatherData) +
+    geom_point(mapping=aes(x=avgTemp, y=relHum, group = 1L , color = season))+
+    labs(title = paste('Humidity (y) vs. Temperature (x) by ____ (animation)'),
          subtitle = 'Season: {closest_state}',
          x = 'Average Temp',
          y = 'Humidity') +
     theme_bw() +
     scale_x_continuous() +
-    scale_y_continuous() +
-    transition_states(states = season,
-                      transition_length = 1,
-                      state_length = 1,
-                      wrap = TRUE)
-  animate(plot5a, nframes = 60 , fps = 10 );
+    scale_y_continuous();
   
-  # 1. Change the plot to animate by month
-  # 2. Change the plot to animate by season
-  # 3. Change the plot so that the point size varies with precipitation
-  # 4. Change the plot so that the color varies with the transition state
-  # 5. Adjust the nframes argument. What happens to your plot output?
-  # 6. Adjust the fps argument. What happens to your plot output?
+  plot5c.1 = plot5c + transition_states(states = season ,
+                                        transition_length = 2,
+                                        state_length = 1,
+                                        wrap = TRUE);
+  
+  animate(plot5c.1, nframes = 60, fps = 10);
   
   # anim_save() -- saving as a gif
-  anim_save(filename = "anim_5a.gif",
-            animation = plot5a,
+  anim_save(filename = "anim_5c.1.gif",
+            animation = plot5c.1,
             nframes = 60,       # number of frames in animation
-            fps = 3);           # frames per second
+            fps = 10);           # frames per second
   
   # anim_save() -- saving as an mp4 video
-  anim_save(filename = "anim_5a.mp4",
-            animation = plot5a,
+  anim_save(filename = "anim_5c.1.mp4",
+            animation = plot5c.1,
             renderer = av_renderer(),
             nframes = 60,       # number of frames in animation
-            fps = 3);           # frames per second
+            fps = 10);           # frames per second
+  
+  # Hints: 
+  # Be careful of calling an excessive number of frames, or frames per second
+  # Make sure that you appropriately map the group
+  
   
   # Notes:
   # - Check out the link below for further tips on the gganimate package
@@ -62,6 +70,6 @@
   # - for continuous variables, it is much harder to control transition times
   # - tweening is the term for how animations handle moving points
   
-
-
+  
+  
 }
--- scripts/WS_part5.R
+++ scripts/WS_part5.R
@@ -7,65 +7,93 @@
   library(package=gganimate);
   library(package=av);          # to create mp4 videos
   
-  abundanceData = read.csv(file="data/final-Charlie.csv", sep=",",
+  # Part 5 Learning goals ####
+  # - Animating with discrete transitions using transition_states (plot5a.1) and transition_reveal (plot5a.2)
+  # - Animating with continuous transitions (plot5b.1)
+  # - saving as GIF
+  # - saving as MP4
+  
+  # First let's load the data...
+  abundanceData = read.csv(file = "data/abundance.csv", sep = ",",
                            header=TRUE, na.strings = c("", NA),
                            stringsAsFactors = FALSE);
   
   apr_to_sept = which(abundanceData$month >= 4 &    
                         abundanceData$month <= 9);   
   
-  coeff_WS_Zoo = max(abundanceData$whitesucker, na.rm=TRUE) /
-    max(abundanceData$zooplankton, na.rm=TRUE);
+  coeff_WS_Zoo = max(abundanceData$whitesucker, na.rm = TRUE) /
+    max(abundanceData$zooplankton, na.rm = TRUE);
   
-  coeff_WB_Zoo = max(abundanceData$whitebass, na.rm=TRUE) /  
-    max(abundanceData$zooplankton, na.rm=TRUE);
+  coeff_WB_Zoo = max(abundanceData$whitebass, na.rm = TRUE) /  
+    max(abundanceData$zooplankton, na.rm = TRUE);
   
-  # Part 5 Learning goals ####
-  # - animations
-  # - saving as GIF
-  # - saving as MP4
-
-  # To animate lines, we need to add "group" to the mapping
-  
-  plot5 = ggplot(data=abundanceData[apr_to_sept,])+
+  #
+  #
+  # Let's go back to the plot we made during Part 4
+  plot5a = ggplot(data=abundanceData[apr_to_sept,])+
     geom_line(mapping=aes(x = month, y = whitesucker, 
-                          group=year, color="White Sucker")) +
+                          group = year, color="White Sucker")) +          # To animate lines, we need to add "group" to the mapping
     geom_line(mapping=aes(x = month, y = zooplankton * coeff_WS_Zoo, 
-                          group=year, color="Zooplankton")) +
-    scale_y_continuous(name = "Larval white sucker abundance",             # first axis
-                       sec.axis = sec_axis(trans = ~./coeff_WS_Zoo,  # second axis
+                          group = year, color="Zooplankton")) +
+    scale_y_continuous(name = "Larval white sucker abundance",          # first axis
+                       sec.axis = sec_axis(trans = ~./coeff_WS_Zoo,     # second axis
                                            name ="Zooplankton abundance")) +
-    labs(title = "Plot 5",
-         subtitle = "Zooplankton vs. Larval white sucker abundance",
-         x="Month",
-         y="Number of Zooplankton",
-         color="Species") +
+    labs(title = "Plot 5a",
+         subtitle = "Zooplankton vs. Larval white sucker abundance: {closest_state}",
+         x = "Month",
+         y = "Number of Zooplankton",
+         color = "Species") +
     theme_bw() +
     scale_x_continuous(breaks = 1:12, 
-                       labels = month.abb)
+                       labels = month.abb);
   
-  plot5.1 = plot5 + transition_states(states = year,         # map animation to the year
-                    transition_length = 1,                 # relative animation time in seconds (default: 1)
-                    state_length = 1,                      # relative length of the pause between states
-                    wrap = TRUE)
-    
-  plot5.2 = plot5 + transition_reveal(along = month) +       # map animation to the month, revealing along that axis
-                  facet_wrap( ~ year);                     # and if you like, facet your plot by year
-
-  animate(plot5, nframes=60);
+  plot5a.1 = plot5a + transition_states(states = year,         # map animation to the year
+                                        transition_length = 1, # relative animation time in seconds (default: 1)
+                                        state_length = 1,      # relative length of the pause between states
+                                        wrap = TRUE);
+  
+  plot5a.2 = plot5a + transition_reveal(along = month) +       # map animation to the month, revealing along that axis
+    facet_wrap( ~ year);                                       # and if you like, facet your plot by year
+  
+  print(animate(plot5a.1, nframes = 60));                      # Number of frames in animation 
+  print(animate(plot5a.2, nframes = 60));                      # Think of how many transitions you need.
   
   #
   #
+  # Now let's try it with a slightly larger dataset, and a different plot type
+  weatherData = read.csv(file = "data/LansingNOAA2016-3.csv", 
+                         stringsAsFactors = FALSE);
   
+  colnames(weatherData)[1] = "day";                           # Rename the column of rownumbers to refer to the "day"
+  weatherData$month = ordered(weatherData$month,              # First, make sure your grouping factors are in the correct order
+                              levels = month.name);
+  weatherData$season = ordered(weatherData$season, 
+                               levels = c("Winter","Spring","Summer","Fall"));
   
+  plot5b = ggplot(data = weatherData) +
+    geom_point(mapping = aes(x = avgTemp, y = relHum, group = precipNum)) +
+    labs(title = paste('Humidity (y) vs. Temperature (x) by Precipitation (animation)'),
+         subtitle = 'Precipitation: {frame_time} inches',
+         x = 'Average Temp', 
+         y = 'Humidity') +
+    theme_bw() +
+    scale_x_continuous() +
+    scale_y_continuous();
+  
+  plot5b.1 = plot5b + transition_time(time = precipNum, # We can transition by a continuous variable too
+                                      range = NULL);    # Range can be changed to limit the "time"
+  
+  print(animate(plot5b.1, nframes = 60)); 
+  
+  # Saving our animated plots 
   # anim_save() also take parameters from animate()
-  anim_save(filename="media/abundance.gif",
-            animation = plot5);
+  anim_save(filename = "media/abundance.gif",
+            animation = plot5a.1);
   
   # anim_save() -- saving as an mp4 video
   anim_save(filename = "media/abundance.mp4",
-            animation = plot5,
+            animation = plot5a.1,
             renderer = av_renderer(),
-            nframes = 60,       # number of frames in animation
+            nframes = 60,       
             fps = 3);           # frames per second
 }
