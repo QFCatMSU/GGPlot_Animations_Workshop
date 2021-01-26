3b2eba8983d8661e47c5c8376babbe2812f744f8
zinkmatt <zinkmat1@msu.edu>
2021-01-26 14:04:41 -0500

    Updated csv and made assignment script

--- scripts/WS_ActivityParts1&2.R
+++ scripts/WS_ActivityParts1&2.R
@@ -7,25 +7,31 @@
                            header=TRUE, na.strings = c("", NA),
                            stringsAsFactors = FALSE);
   
-  # Parts 1&2 Assignment Key ####
+  #tasks
+  #Subset your data from march to october for 2010
+  #map the larval white bass abundance to the y axis
+  #used a dashed line
+  #Make this line blue
+  #pick a theme different from the default
+  #Label and sublabel your plot
   
   # subset data and color is used as a style change here -- it is not mapped to data
   year2010_sub = which(abundanceData$year == 2010 &
-                     abundanceData$month>=3 &  #3rd month is March
-                     abundanceData$month<=10); #10th month is October
+                         abundanceData$month>=3 &  #3rd month is March
+                         abundanceData$month<=10); #10th month is October
   
   solution = ggplot(data=abundanceData[year2010_sub,])+
-    geom_line(mapping=aes(x=month, y=whitebass), #be sure to map the correct column the y-axis
+    geom_line(mapping=aes(x=month, y=zooplankton), #be sure to map the correct column the y-axis
               color="blue",
               size=2,
               linetype="dashed") +   # linetypes: http://www.sthda.com/english/wiki/ggplot2-line-types-how-to-change-line-types-of-a-graph-in-r-software
     theme_classic() + #any theme of your choosing works here.
     scale_x_continuous(breaks=1:12, # Different way to display months
                        labels= month.abb) +
-    labs(title="Larval White Bass Abundance",#Use title and subtitle to label the plot correctly.
+    labs(title="zooplankton Abundance",#Use title and subtitle to label the plot correctly.
          subtitle="2010 Mar - Oct", 
-         y="Number of Larval White Bass");
+         y="zooplankton");
   plot(solution);
   
-
+  
 }
