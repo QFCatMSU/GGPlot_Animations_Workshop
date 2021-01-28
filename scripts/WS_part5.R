ee75eb1ac9389f6edce98fdd0d8ae9b5c96aa01f
cmbuckley93 <buckl123@msu.edu>
2021-01-28 13:44:48 -0700

    fixes

--- scripts/WS_part5.R
+++ scripts/WS_part5.R
@@ -8,7 +8,8 @@
   library(package=av);          # to create mp4 videos
   
   # Part 5 Learning goals ####
-  # - Animating with discrete transitions using transition_states (plot5a.1) and transition_reveal (plot5a.2)
+  # - Animating with discrete transitions using transition_states (plot5a.1) 
+  #        and transition_reveal (plot5a.2)
   # - Animating with continuous transitions (plot5b.1)
   # - saving as GIF
   # - saving as MP4
@@ -27,14 +28,11 @@
   coeff_WB_Zoo = max(abundanceData$whitebass, na.rm = TRUE) /  
     max(abundanceData$zooplankton, na.rm = TRUE);
   
-<<<<<<< HEAD
   month = ordered(abundanceData$month, levels = month.abb[])
   
-=======
->>>>>>> 3484fa906c2acc18e12b82436a54d63104c1efdd
   #
   #
-  # Let's go back to the plot we made during Part 4
+  # Let's start with a similar plot to what we made during Part 4, but this is incomplete!
   plot5a = ggplot(data=abundanceData[apr_to_sept,])+
     geom_line(mapping=aes(x = month, y = whitesucker, 
                           group = year, color="White Sucker")) +          # To animate lines, we need to add "group" to the mapping
@@ -42,35 +40,31 @@
                           group = year, color="Zooplankton")) +
     scale_y_continuous(name = "Larval white sucker abundance",          # first axis
                        sec.axis = sec_axis(trans = ~./coeff_WS_Zoo,     # second axis
-<<<<<<< HEAD
                                            name = "Zooplankton abundance",
                                            labels = scales::comma),
                        labels = scales::comma);
+  
+  plot(plot5a)
+  
   #
   #
   # Now we have a basic plot, lets add an animation component 
   plot5a.1 = plot5a +
     labs(title = "Plot 5a.1",
-=======
-                                           name ="Zooplankton abundance")) +
-    labs(title = "Plot 5a",
->>>>>>> 3484fa906c2acc18e12b82436a54d63104c1efdd
-         subtitle = "Zooplankton vs. Larval white sucker abundance: {closest_state}",
+         subtitle = "Zooplankton vs. Larval white sucker abundance: {closest_state}", # {closest_state} will refer to the varying state
          x = "Month",
          y = "Number of Zooplankton",
          color = "Species") +
     theme_bw() +
     scale_x_continuous(breaks = 1:12, 
-                       labels = month.abb);
-  #
-  #
-  # Now we have a basic plot, lets add an animation component 
-  plot5a.1 = plot5a + transition_states(states = year,         # map animation to the year
-                                        transition_length = 1, # relative animation time in seconds (default: 1)
-                                        state_length = 1,      # relative length of the pause between states
-                                        wrap = TRUE);
+                       labels = month.abb)+
+    transition_states(states = year,         # map animation to the year. Each year is a single frame!
+                      transition_length = 1, # relative animation time in seconds (default: 1)
+                      state_length = 1,      # relative length of the pause between states
+                      wrap = TRUE);
   
-<<<<<<< HEAD
+  animate(plot5a.1, nframes = 60, fps = 3);    
+  
   plot5a.2 = plot5a + transition_reveal(along = month) +
     labs(title = "Plot 5a.2",
          subtitle = "Zooplankton vs. Larval white sucker abundance ",
@@ -80,35 +74,21 @@
     theme_bw() +
     scale_x_continuous(breaks = 1:12, 
                        labels = month.abb)+       # map animation to the month, revealing along that axis
-    facet_wrap( ~ year);                                       # and if you like, facet your plot by year
+    facet_wrap( ~ year);                          # and if you like, facet your plot by year
   
-  animate(plot5a.1, nframes = 60);                      # Number of frames in animation 
-  animate(plot5a.2, nframes = 60);                     
-=======
-  plot5a.2 = plot5a + transition_reveal(along = month) +       # map animation to the month, revealing along that axis
-    facet_wrap( ~ year);                                       # and if you like, facet your plot by year
-  
-  print(animate(plot5a.1, nframes = 60));                      # Number of frames in animation 
-  print(animate(plot5a.2, nframes = 60));                      # Think of how many transitions you need.
->>>>>>> 3484fa906c2acc18e12b82436a54d63104c1efdd
+                    
+  animate(plot5a.2, nframes = 60, fps=3);  # Number of frames in animation                    
   
   #
+  # Lets take a second...
+  
   #
   # Now let's try it with a slightly larger dataset, and a different plot type
-  weatherData = read.csv(file = "data/LansingNOAA2016-3.csv", 
-                         stringsAsFactors = FALSE);
+  weatherData = read.csv(file = "data/LansingNOAA2016-3.csv", stringsAsFactors = FALSE);
   
-<<<<<<< HEAD
   day = seq(1:nrow(weatherData));                          # Rename the column of rownumbers to refer to the "day"
   month = ordered(weatherData$month, levels = month.abb[]);    # First, make sure your grouping factors are in the correct order
   season = ordered(weatherData$season, levels = c("Winter","Spring","Summer","Fall"));
-=======
-  colnames(weatherData)[1] = "day";                           # Rename the column of rownumbers to refer to the "day"
-  weatherData$month = ordered(weatherData$month,              # First, make sure your grouping factors are in the correct order
-                              levels = month.name);
-  weatherData$season = ordered(weatherData$season, 
-                               levels = c("Winter","Spring","Summer","Fall"));
->>>>>>> 3484fa906c2acc18e12b82436a54d63104c1efdd
   
   plot5b = ggplot(data = weatherData) +
     geom_point(mapping = aes(x = avgTemp, y = relHum, group = precipNum)) +
@@ -120,33 +100,27 @@
     scale_x_continuous() +
     scale_y_continuous();
   
-<<<<<<< HEAD
   plot(plot5b)
   
   plot5b.1 = plot5b + transition_time(time = precipNum,      # We can transition by a continuous variable too
                                       range = NULL);         # Range can be changed to limit the "time"
-=======
-  plot5b.1 = plot5b + transition_time(time = precipNum, # We can transition by a continuous variable too
-                                      range = NULL);    # Range can be changed to limit the "time"
->>>>>>> 3484fa906c2acc18e12b82436a54d63104c1efdd
   
   animate(plot5b.1, nframes = 60); 
   
-<<<<<<< HEAD
- 
-   # Saving our animated plots 
-  # anim_save() can also take parameters from animate()
-=======
   # Saving our animated plots 
-  # anim_save() also take parameters from animate()
->>>>>>> 3484fa906c2acc18e12b82436a54d63104c1efdd
+  # anim_save() can also take parameters from animate()
+  # If on a Mac these files won't be read with your default viewer
+  #      -> use your browser to view them!
+  
   anim_save(filename = "media/abundance.gif",
-            animation = plot5a.1);
+            animation = plot5a.1,
+            nframes = 60,
+            fps = 3);
   
   # anim_save() -- saving as an mp4 video
   anim_save(filename = "media/abundance.mp4",
             animation = plot5a.1,
             renderer = av_renderer(),
             nframes = 60,       
-            fps = 3);           # frames per second
+            fps = 3); 
 }
